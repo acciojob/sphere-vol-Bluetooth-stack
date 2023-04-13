5143fb5563a7da8331d773185b3de531a3771812
Siddharth Hota <80689111+Bluetooth-stack@users.noreply.github.com>
2023-04-14 01:15:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-
-function volume_sphere() {
-    //Write your code here
-	let radius = document.querySelector('#radius').value;
-	let vol = document.querySelector('#volume');
-	let result = 4/3 * (Math.PI) * (Math.pow(radius, 3));
-	// alert(result);
-	vol.value= result;
-	// console.log(vol);
-	return false;
-} 
-
+function volume_sphere()
+ {
+  var volume;
+  var radius = document.getElementById('radius').value;
+  radius = Math.abs(radius);
+  volume = (4/3) * Math.PI * Math.pow(radius, 3);
+  volume = volume.toFixed(4);
+  document.getElementById('volume').value = volume;
+  return false;
+ } 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
