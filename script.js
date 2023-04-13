aa2ad3e802ec92262bdd442c60ff37b40f317d23
Siddharth Hota <80689111+Bluetooth-stack@users.noreply.github.com>
2023-04-14 01:01:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-const but = document.querySelector('#submit');
-	let vol = document.querySelector('#volume').value;
-// but.addEventListener('click', volume_sphere) 
+
 function volume_sphere() {
     //Write your code here
-	const radius = document.querySelector('#radius').value;
-	// vol.disabled = true;
-	// let result = 4/3 * (3.141) * (Math.pow(radius, 3));
+	let radius = document.querySelector('#radius').value;
+	let vol = document.querySelector('#volume');
+	let result = 4/3 * (3.141) * (Math.pow(radius, 3));
 	// alert(result);
-	vol= 4/3 * (3.141) * (Math.pow(radius, 3));
+	vol.value= result;
 	// console.log(vol);
+	return false;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
